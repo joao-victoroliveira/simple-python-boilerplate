843740f402c5ae2cb888df08686c390d763025be
João Victor <joaovictor.neto@thoughtworks.com>
2019-08-20 15:57:49 -0300

    Estrutura inicial de lógico de negócio para definir o Hotel com menor preço

---
+++ src/hotel_book_selector.py
@@ -0,0 +1,17 @@
+class HotelBookSelector:
+    
+    def __init__(self, input_raw):
+        # tipo de usuario, datas
+        self.hotels = [
+
+        ]
+        self.arrayPrecos = []
+    
+    def build_proper_input(self):
+        self
+
+    def getPriceArray(self):
+        self
+
+    def wiiner(self):
+        self
