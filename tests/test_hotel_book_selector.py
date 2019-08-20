7e7a57137fd9ee83529b6a775f15d471e867dcd6
João Victor <joaovictor.neto@thoughtworks.com>
2019-08-20 15:58:55 -0300

    Exemplo de teste unitário utilizando o PyTest

---
+++ tests/test_hotel_book_selector.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from context import src
+from src.hotel import HotelBookSelector
+
+class HotelBookSelectorTest(TestCase):
+    def test(self):
+        result = 1
+        self.assertEqual(result, 1)
+
+    def testObject(self):
+        hotel = HotelBookSelector([])
+        self.assertEqual(hotel.message(),'Hello!')
