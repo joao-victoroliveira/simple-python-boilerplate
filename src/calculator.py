94261e6c0808bf0e0990deeb45f967e3fcb1083a
joao-victoroliveira <joaovictor.neto@thoughtworks.com>
2019-09-06 08:53:38 -0300

    Implementação da classe calculator para definir, dado uma lista de hotel, qual dos hotéis possui o menor preço para estadia

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,15 @@
 class Calculator:
-    def getCheappest(self):
-        return ''
+    def __init__(self, hotels = []):
+        self.hotels = hotels
+
+    def getCheappest(self, customerType, period):
+        cheapestHotel = None
+        if self.hotels == []:
+            return ''        
+        cheapestHotel = self.hotels[0]
+        for hotel in self.hotels:
+            current_cheapest = cheapestHotel.getPriceForPeriod(customerType, period)
+            hotel_price = hotel.getPriceForPeriod(customerType, period)
+            if current_cheapest > hotel_price or (current_cheapest == hotel_price and cheapestHotel.rating < hotel.rating):
+                cheapestHotel = hotel
+        return cheapestHotel.name
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,31 @@
 from unittest import TestCase
 from context import src
+from src.hotel import Hotel
 from src.calculator import Calculator
 
+customerType, period = 'regular',['SUN','MON','TUE']
 class CalculatorTest(TestCase):
     
     def test_should_return_name_of_the_cheapper_hotel(self):
         calculator = Calculator()
-        self.assertEqual(calculator.getCheappest(), '')
+        self.assertEqual(calculator.getCheappest(customerType, period), '')
+    
+    def test_should_return_name_of_the_cheapper_hotel_when_pass_one_hotel(self):
+        hotels = [Hotel('LakeWood', 8, 10, 20, 25, 1)]
+        calculator = Calculator(hotels)
+        self.assertEqual(calculator.getCheappest(customerType, period), 'LakeWood')
+
+    def test_should_return_name_of_the_cheapper_hotel_when_pass_two_hotels(self):
+        hotels = [Hotel('LakeWood', 8, 10, 20, 25, 1), Hotel('Wood', 10, 20, 20, 25, 1)]
+        calculator = Calculator(hotels)
+        self.assertEqual(calculator.getCheappest(customerType, period), 'LakeWood')
+
+    def test_should_return_name_of_the_second_hotel_because_its_cheaper(self):
+        hotels = [Hotel('Wood', 10, 20, 20, 25, 1), Hotel('LakeWood', 8, 10, 20, 25, 1)]
+        calculator = Calculator(hotels)
+        self.assertEqual(calculator.getCheappest(customerType, period), 'LakeWood')
+
+    def test_should_return_name_of_the_cheapper_hotel_when_pass_two_hotels_with_same_price(self):
+        hotels = [Hotel('LakeWood', 10, 20, 20, 25, 1), Hotel('Wood', 10, 20, 20, 25, 2)]
+        calculator = Calculator(hotels)
+        self.assertEqual(calculator.getCheappest(customerType, period), 'Wood')
